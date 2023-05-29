3d9bcfcfc06546d2c9662368620944455046e5af
Akinbowale Akinyemi <akin.akinyemi81@gmail.com>
2023-05-29 05:37:53 -0700

    module in alphabetical order

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
+""" This script queries the Reddit API and returns the number of subscribers
+    of the passed in subreddit """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    try:
+        subscribers = requests.get('https://www.reddit.com/r/{}/about.json'.
+                                   format(subreddit), allow_redirects=False,
+                                   headers={'User-Agent': 'custom'})
+        return subscribers.json().get('data').get('subscribers')
+    except:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
